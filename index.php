7d925c514985a4e6d243def7911c1b1b73cc8a69
pimpimpro <quang123hyhy@gmail.com>
2023-11-24 22:21:39 +0700

    phân trang,lọc giá,giỏ hàng

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ include "global.php";
 include "model/taikhoan.php";
 include "model/sanpham.php";
 include "model/danhmuc.php";
-$allProduct=load_all_product();
-$allCategory=loadall_danhmuc();
-$product_adidas=loadall_danhmuc("",1);
+if (!isset($_SESSION['mycart'])) {
+    $_SESSION['mycart'] = [];
+}
+$conn = pdo_get_connection();
+// $allProduct=load_all_product();
+$allCategory = loadall_danhmuc();
+$product_adidas = loadall_danhmuc("", 1);
 include "view/header.php";
 if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
@@ -44,62 +48,104 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
             include "view/register.php";
             break;
         case "dangnhap":
-            if(isset($_POST['dangnhap'])&&($_POST['dangnhap']!="")){
+            if (isset($_POST['dangnhap']) && ($_POST['dangnhap'] != "")) {
                 $user = $_POST['username'];
                 $pass = $_POST['password'];
-                $login=dangnhap($user,$pass);
-                if(is_array($login)){
-                    $_SESSION['user']=$login;
+                $login = dangnhap($user, $pass);
+                if (is_array($login)) {
+                    $_SESSION['user'] = $login;
                     header('Location: index.php');
-                    $thongbao='<script>alert("Đăng Nhập Thành Công")</script>';
-                }else{
-                    $thongbaoerr= '<i class="fas fa-exclamation-circle" style="color: #ff0000;margin-right:10px;"></i>Tài Khoản Hoặc Mật Khẩu Sai';
-                // header('Location: index.php?act=dangnhap');
+                    $thongbao = '<script>alert("Đăng Nhập Thành Công")</script>';
+                } else {
+                    $thongbaoerr = '<i class="fas fa-exclamation-circle" style="color: #ff0000;margin-right:10px;"></i>Tài Khoản Hoặc Mật Khẩu Sai';
+                    // header('Location: index.php?act=dangnhap');
                 }
             }
             include "view/login.php";
             break;
         case "dangxuat":
-        
+
             session_unset();
             header('Location: index.php');
             break;
         case "loadallsp":
-            include "view/loadproduct.php";
+            include "view/loadproduct2.php";
             break;
         case "product_category":
-            if(isset($_GET['id_category']) && ($_GET['id_category']>0) ){
-                $id_category=$_GET['id_category'];
-                $dssp=loadall_sanpham("", $id_category);
+            if (isset($_GET['id_category']) && ($_GET['id_category'] > 0)) {
+                $id_category = $_GET['id_category'];
+                $dssp = loadall_sanpham("", $id_category);
                 include "view/product_category.php";
             }
             break;
         case "product_detail":
-            if(isset($_GET['id_product']) && ($_GET['id_product']>0)){
-                $id=$_GET['id_product'];
-                $product_detail=loadone_sanpham($id);
+            if (isset($_GET['id_product']) && ($_GET['id_product'] > 0)) {
+                $id = $_GET['id_product'];
+                $product_detail = loadone_sanpham($id);
+                update_view($id);
                 include "view/chitietsanpham.php";
                 break;
-            }else {
+            } else {
                 include "view/trangchu.php";
             }
-            case "buy_now":
-                if(isset($_GET['id_product'])&& ($_GET['id_product']>0)){
-                    $id=$_GET['id_product'];
-                    $product_detail=loadone_sanpham($id);
-                }
-              include "view/buy-now.php";
-              break;
+            break;
+        case "addtocart":
+            if (isset($_POST['addtocart']) && ($_POST['addtocart'])) {
+                $id_product = $_POST['id_product'];
+                $product_name = $_POST['product_name'];
+                $image = $_POST['image'];
+                $price = $_POST['price'];
+                $soluong = 1;
+                $total_price = $soluong * $price;
+                $productadd = [$id_product, $product_name, $image, $price, $soluong, $total_price];
+                array_push($_SESSION['mycart'], $productadd);
+            }
+            include "view/cart/viewcart.php";
+            break;
+        case "delete_cart":
+            if (isset($_GET['idcart'])) {
+                array_splice($_SESSION['mycart'], $_GET['idcart'], 1);
+            } else {
+                $_SESSION['mycart'] = [];
+            }
+            header('location: index.php?act=viewcart');
+            break;
+        case "viewcart":
+            include "view/cart/viewcart.php";
+            break;
+        case "buy_now":
+            if (isset($_GET['id_product']) && ($_GET['id_product'] > 0)) {
+                $id = $_GET['id_product'];
+                $product_detail = loadone_sanpham($id);
+            }
+            include "view/buy-now.php";
+            break;
 
-              case "buy_now_result":
-                if(isset($_POST['order_click'])&&($_POST['order_click'])){
-                    $thongbao="Mua Hàng Thành Công";
-                    include "view/buy_now_result.php";
+        case "buy_now_result":
+            if (isset($_POST['order_click']) && ($_POST['order_click'])) {
+                $thongbao = "Mua Hàng Thành Công";
+                include "view/buy_now_result.php";
+            } else {
+                include "view/buy-now.php";
+            }
+
+            break;
+        case "filter-price":
+            if (isset($_POST['search-pice']) && ($_POST['search-pice'])) {
+                $min=$_POST['price-min'];
+                $max=$_POST['price-max'];
+                if($min>$max || $min=='' || $max ==''){
+                    $errFilter="Nhập đúng dữ liệu !";
+                    include "view/loadproduct2.php";
+                    break;
                 }else{
-                    include "view/buy-now.php";  
+                    $filter_products=filter_price_product($min,$max);
+                    include "view/loadproduct.php";
                 }
-               
-                break;
+            }else{
+                include "view/loadproduct2.php";
+            }
+            break;
     }
 } else {
     include "view/trangchu.php";
