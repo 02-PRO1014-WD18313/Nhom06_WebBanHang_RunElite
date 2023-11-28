7dd9efd7a17983f7259e3b5a60444d5a9606139d
vnvtn8@gmail.com <vnvtn8@gmail.com>
2023-11-28 22:21:47 +0700

    update28/11/10h21

--- model/taikhoan.php
+++ model/taikhoan.php
@@ -1,4 +1,36 @@
+
 <?php
+
+// update_taikhoan($id_user, $tentk, $password, $gmail, $role);
+function update_taikhoan($id_user, $username, $password, $gmail, $phone, $role, $address )
+{
+   
+        $sql = "update user set id_user='" . $id_user . "', username='" . $username . "', password='" . $password . "'  ,
+        gmail='" . $gmail . "', phone='" . $phone . "', role='" . $role . "', address='" . $address . "'
+         where id_user=" . $id_user;
+
+    pdo_execute($sql);
+}
+
+function loadone_taikhoan($id)
+{
+    $sql = "select * from user where id_user=" . $id;
+    $taikhoan = pdo_query_one($sql);
+    return $taikhoan;
+}
+
+function loadall_taikhoan(){
+    $sql="select * from user";
+    $listtaikhoan = pdo_query($sql);
+    return  $listtaikhoan;
+}
+
+function delete_taikhoan($id)
+{
+    $sql = "delete from user where id_user =" . $id;
+    pdo_execute($sql);
+}
+
 function themtaikhoan($user,$pass,$email){
     $sql="INSERT INTO user (username,password,gmail) VALUES ( '$user', '$pass','$email')";
     pdo_execute($sql);
